ded499f3e82a5ecd0274a9ecdd7ce0caca864c69
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-02-10 17:33:35 +0100

    Typy klasy i obiekty - init

---
+++ POJ/1Klasy/CommandLine.java
@@ -0,0 +1,9 @@
+// Przyklad dostepu do parametrow wywolania
+
+class CommandLine {
+   public static void main(String args[]) {
+     for(int i = 0; i < args.length; i++)
+       System.out.println("arg[" + i + "] =" + 
+                           args[i]);
+   }
+}
