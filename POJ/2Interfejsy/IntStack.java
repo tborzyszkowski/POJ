f3144b664b987668d28de622994e30f674aa2a7d
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-02-10 20:58:55 +0100

    Interfejsy - init

---
+++ POJ/2Interfejsy/IntStack.java
@@ -0,0 +1,7 @@
+// definicja interfejsu stosow liczb calkowitych
+interface IntStack {
+    void push(int item); // element na stos
+    int pop();           // zdejmij element ze stosu
+}
+// st.push(x);
+// st.pop() == x
---
+++ POJ/2Interfejsy/InterfejsyEx/IfaceImpl.java
@@ -0,0 +1,18 @@
+package InterfejsyEx;
+
+interface Wymaganie{
+	void f();
+}
+class A{
+	public void f(){}
+}
+
+class B extends A implements Wymaganie{	
+}
+public class IfaceImpl {
+	public static void main(String[] args) {
+		Wymaganie zm  = new B();
+		//Wymaganie zm2 = new A();
+		zm.f();
+	}
+}
