63ce89e1bece38523e37feaee7da6b63e032a69a
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-02-11 11:59:04 +0100

    Pakiety - init

---
+++ POJ/4Pakiety/protection/p3/Protection3.java
@@ -0,0 +1,10 @@
+package protection.p3;
+
+
+class Protection3 extends protection.p2.Protection2 {
+
+	public Protection3() {
+		
+	}
+
+}
---
+++ POJ/4Pakiety/test/PackTest.java
@@ -0,0 +1,8 @@
+// przyklad definicji pakietu
+package test;
+
+public class PackTest {
+	public static void main(String args[]) {
+		System.out.println("\nTest klasy PackTest\n");
+	}
+}
