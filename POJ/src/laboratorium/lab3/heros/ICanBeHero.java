8290879521ffce4cf4b447555819098b4d39efe4
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-04-16 12:24:51 +0200

    Heros example

---
+++ POJ/src/laboratorium/lab3/heros/ICanBeHero.java
@@ -0,0 +1,21 @@
+package laboratorium.lab3.heros;
+
+public interface ICanBeHero {
+
+	public int getStrength();
+
+	public int getDefence();
+
+	public int getIntelligence();
+
+	public int getDextrity();
+
+	public int getAgility();
+
+	public int getSpeed();
+
+	public String getName();
+
+	public void setName(String name);
+
+}
