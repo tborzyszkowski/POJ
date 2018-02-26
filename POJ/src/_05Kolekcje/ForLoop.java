d1ee84bc611b4131d07bc38da602d7b381f6e881
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-02-26 15:04:22 +0100

    Kolekcje

---
+++ POJ/src/_05Kolekcje/ForLoop.java
@@ -0,0 +1,35 @@
+package _05Kolekcje;
+
+import java.util.ArrayList;
+
+class ForLoop {
+	public static void main(String args[]) {
+		double a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		double result = 0;
+
+		// normalna petla for
+		for (int i = 0; i < a.length; i++)
+			result += a[i];
+		System.out.println(result);
+
+		// inna forma petli for
+		result = 0;
+		for (double el : a)
+			result += el;
+		System.out.println(result);
+
+		ArrayList<String> al = new ArrayList<String>();
+
+		al.add("C");
+		al.add("A");
+		al.add("E");
+		al.add("B");
+		al.add("D");
+		al.add("F");
+		al.add(1, "A2");
+		
+		for (String v : al)
+			System.out.println(v);
+	}
+	
+}
---
+++ POJ/src/_05Kolekcje/Stack_1G.java
@@ -0,0 +1,59 @@
+package _05Kolekcje;
+
+// implementacja stosow w oparciu o kolekcje
+// wykorzystanie typow parametrycznych
+import java.util.*;
+
+class Stack33<T> {
+	private ArrayList<T> items;
+
+	public Stack33(int size) {
+		items = new ArrayList<T>(size);
+	}
+
+	void push(T item) {
+		items.add(item);
+	}
+
+	public T pop() {
+		int len = items.size();
+		T obj = null;
+		if (len == 0)
+			throw new EmptyStackException();
+		obj = items.get(len - 1);
+		items.remove(len - 1);
+		return obj;
+	}
+
+	public boolean isEmpty() {
+		return items.isEmpty();
+	}
+	
+	public int size(){
+		return items.size();
+	}
+}
+
+// Klasa testujaca stos
+class Stack_1G {
+	public static void main(String args[]) {
+		Stack33<Number> myStack = new Stack33<>(10);
+
+		// wkladamy liczby na stos
+		for (int i = 0; i < 15; i++)
+			myStack.push(new Integer(i));
+
+		// TU otrzymamy blad
+		 myStack.push(new Long(5));
+		// myStack.push("Ala ma kota");
+		
+		System.out.println("Czy stos jest pusty? : " + myStack.isEmpty());
+
+		// zdejmujemy ze stosu
+		System.out.println("Stos myStack:");
+		for (int i = 0; i < myStack.size(); i++)
+			System.out.println(myStack.pop().intValue()+1);
+
+		System.out.println("Czy stos jest pusty? : " + myStack.isEmpty());
+	}
+}
