9c12b6a7bac341699fb84db827c1bd947c8889f6
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-02-26 15:06:45 +0100

    Wyjątki

---
+++ POJ/src/_06Wyjatki/Exc9b.java
@@ -0,0 +1,25 @@
+package _06Wyjatki;
+
+class Exc9b {
+	static void throwOne() throws IllegalAccessException {
+		System.out.println("Wewnątrz metody");
+		throw new IllegalAccessException("To tylko test");
+	}
+
+	public static void main(String args[]) //throws IllegalAccessException
+	{
+		try {
+			throwOne();
+			int a = 1 /0;
+		} 
+		catch (IllegalAccessException e) {
+			System.out.println("Obsłużyłem: ");
+			e.printStackTrace();
+			System.out.println("Komunikat wyjątku  : " + e.getMessage());
+			//throw e;
+		} 		
+		catch (ArithmeticException e) {}
+		catch (Exception e) {}
+
+	}
+}
