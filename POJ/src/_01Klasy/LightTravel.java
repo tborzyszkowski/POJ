7e6f16203be9529db7ac734c4227996ad58cfa6c
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-02-27 13:41:40 +0100

    Po wykładzie

--- POJ/src/_01Klasy/LightTravel.java
+++ POJ/src/_01Klasy/LightTravel.java
@@ -5,7 +5,7 @@ package _01Klasy;
 class LightTravel{
     public static void main(String args[]){
 	int lightSpeed = 299792; // km/s
-	long days = 1000000000;        // badana liczba dni
+	long days = 100000000;        // badana liczba dni
 	long seconds = days * 24 * 60 * 60;   // tyle to sekund
 	long distance = lightSpeed * seconds; // tyle przebedzie swiatlo
 
