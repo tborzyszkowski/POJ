811ec40cd07315cb521ca5150840ea3392f2b08d
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-03-13 09:39:21 +0100

    koszyk

---
+++ POJ/src/laboratorium/lab2/koszyk/Main.java
@@ -0,0 +1,27 @@
+package laboratorium.lab2.koszyk;
+
+import java.util.ArrayList;
+import laboratorium.lab2.koszyk.model.*;
+
+public class Main {
+	public static ArrayList<Product> getSampleProducts(int n){
+		ArrayList<Product> sample = new ArrayList<>();
+		for(int i = 1; i <= n; i++){
+			Product p = new Product("Product "+ i, i * 25);
+			sample.add(p);
+		}
+		return sample;
+	}
+	
+	public static Cart getSampleCart(){
+		Cart c = new Cart();
+		for(Product p : getSampleProducts(10)){
+			c.getItems().add(new CartItem(p));
+		}
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getSampleCart().getItems());
+	}
+}
