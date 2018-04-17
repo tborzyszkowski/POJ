0afd04c1a716f1295a1241b7cb20d04ba434a5f0
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-04-17 10:07:58 +0200

    Samochody

---
+++ POJ/src/laboratorium/lab5/samochody/RocznikComparator.java
@@ -0,0 +1,18 @@
+package laboratorium.lab5.samochody;
+
+import java.util.Comparator;
+
+public class RocznikComparator implements Comparator<Ogloszenie> {
+
+	@Override
+	public int compare(Ogloszenie o1, Ogloszenie o2) {
+		if (o1.getSamochod().getRocznik() > o2.getSamochod().getRocznik())
+			return 1;
+		else if (o1.getSamochod().getRocznik() < o2.getSamochod().getRocznik())
+			return -1;
+		else
+			return 0;
+
+	}
+
+}
---
+++ POJ/src/laboratorium/lab5/samochody/Samochod.java
@@ -0,0 +1,54 @@
+package laboratorium.lab5.samochody;
+
+public class Samochod {
+	private String marka;
+	private String model;
+	private int rocznik;
+	private int przebieg;
+
+	public Samochod(String marka, String model, int rocznik, int przebieg) {
+		super();
+		this.marka = marka;
+		this.model = model;
+		this.rocznik = rocznik;
+		this.przebieg = przebieg;
+	}
+
+	public String getMarka() {
+		return marka;
+	}
+
+	public void setMarka(String marka) {
+		this.marka = marka;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getRocznik() {
+		return rocznik;
+	}
+
+	public void setRocznik(int rocznik) {
+		this.rocznik = rocznik;
+	}
+
+	public int getPrzebieg() {
+		return przebieg;
+	}
+
+	public void setPrzebieg(int przebieg) {
+		this.przebieg = przebieg;
+	}
+
+	@Override
+	public String toString() {
+		return "Samochod [marka=" + marka + ", model=" + model + ", rocznik=" + rocznik + ", przebieg=" + przebieg
+				+ "]";
+	}
+}
