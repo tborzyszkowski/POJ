0de3d148b3c1aaf4d38eb7b366abc17395c17bff
tborzyszkowski <t.borzyszkowski@gmail.com>
2019-02-23 16:07:56 +0100

    FakeTest

---
+++ POJ/src/laboratorium/lab1/test/FakeTest.java
@@ -0,0 +1,14 @@
+package laboratorium.lab1.test;
+
+public class FakeTest {
+
+	public static void main(String[] args) {
+		Fake f = new Fake(5);
+		
+		f.setX(6);
+		
+		System.out.println(f);
+
+	}
+
+}
